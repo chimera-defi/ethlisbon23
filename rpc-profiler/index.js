a023ec301c53b41584b48d518714b5789a30cbd1
Chimera <81845287+chimera-defi@users.noreply.github.com>
2023-05-14 09:55:16 +0100

    Add files via upload

---
+++ rpc-profiler/index.js
@@ -0,0 +1,121 @@
+const { ALCHEMY_KEY, INFURA_KEY, CHAINSTACK_ENDPOINT, LLAMA_NODES_KEY, TENDERLY_KEY, QUICKNODE_ENDPOINT } = require("./secrets");
+
+const ALCHEMY_URL = `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`;
+const INFURA_URL = `https://mainnet.infura.io/v3/${INFURA_KEY}`;
+const CHAINSTACK_URL = `${CHAINSTACK_ENDPOINT}`;
+// const LLAMA_NODES_URL = `https://eth.llamarpc.com/rpc/${LLAMA_NODES_KEY}`;
+const LLAMA_NODES_URL = 'https://eth.llamarpc.com';
+const TENDERLY_URL = `https://mainnet.gateway.tenderly.co/${TENDERLY_KEY}`
+const QUICKNODE_URL = `${QUICKNODE_ENDPOINT}`;
+const GASHACK = 'https://beta-be.gashawk.io:3001/proxy/rpc';
+const b69 = 'https://rpc.builder0x69.io/';
+
+const RUN_TIME = 1 * 60 * 1000; // 5 minutes
+const CONCURRENT_REQUESTS = 100; // 1,000
+
+const ethers = require("ethers");
+
+const getContract = (providerUrl) => {
+  const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+  const contractAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+  const BALANCE = "function balanceOf(address owner) view returns (uint256)";
+  const contractAbi = [BALANCE];
+  return new ethers.Contract(contractAddress, contractAbi, provider);
+};
+
+const canGetbalance = async (contract) => {
+  try {
+    const USER_ADDRESS = "0x477b8D5eF7C2C42DB84deB555419cd817c336b6F";
+    const balance = await contract.balanceOf(USER_ADDRESS);
+    const balanceInUsd = ethers.utils.formatUnits(balance, 6);
+    return balanceInUsd > 0;
+  } catch (error) {
+    return false;
+  }
+};
+
+const testSpeed = async (providerUrl) => {
+  if (providerUrl.length == 0) return 'No provider';
+
+  const contract = getContract(providerUrl);
+  let testing = true;
+
+  setTimeout(() => {
+    testing = false;
+  }, RUN_TIME);
+
+  let successCount = 0;
+  let errorCount = 0;
+  while (testing) {
+    const success = await canGetbalance(contract);
+    if (success) successCount++;
+    else errorCount++;
+  }
+  return { successCount, errorCount };
+};
+
+const testConcurrentRequests = async (providerUrl) => {
+  if (providerUrl.length == 0) return 'No provider';
+
+  const startTime = Date.now();
+  const contract = getContract(providerUrl);
+  const requests = Array.from({ length: CONCURRENT_REQUESTS }, () =>
+    canGetbalance(contract)
+  );
+  const results = await Promise.all(requests);
+  const successCount = results.filter((result) => result).length;
+  const errorCount = results.filter((result) => !result).length;
+  const endTime = Date.now();
+  const timeElapsed = endTime - startTime;
+  return { successCount, errorCount, timeElapsed: `${timeElapsed / 1000}s` };
+};
+
+(async function main() {
+  //   // Speed tests
+    // await testSpeed(ALCHEMY_URL).then((result) => {
+    //   console.log("Alchemy Speed: ", result);
+    // });
+    await testSpeed(INFURA_URL).then((result) => {
+      console.log("Infura Speed: ", result);
+    });
+    await testSpeed(CHAINSTACK_URL).then((result) => {
+      console.log("sharedtools Speed: ", result);
+    });
+    await testSpeed(LLAMA_NODES_URL).then((result) => {
+      console.log("Llama Nodes Speed: ", result);
+    });
+
+    await testSpeed(GASHACK).then((result) => {
+      console.log("GasHawk Nodes Speed: ", result);
+    });
+
+    await testSpeed(b69).then((result) => {
+      console.log("builder 69 Nodes Speed: ", result);
+    });
+    // await testSpeed(TENDERLY_URL).then((result) => {
+    //   console.log("Tenderly Web3 Gateway Speed: ", result);
+    // });
+    // await testSpeed(QUICKNODE_URL).then((result) => {
+    //   console.log("QuickNode Speed: ", result);
+    // });
+
+  // Concurrent requests tests
+  // await testConcurrentRequests(ALCHEMY_URL).then((result) => {
+  //   console.log("Alchemy Concurrent Requests: ", result);
+  // });
+  // await testConcurrentRequests(INFURA_URL).then((result) => {
+  //   console.log("Infura Concurrent Requests: ", result);
+  // });
+  // await testConcurrentRequests(CHAINSTACK_URL).then((result) => {
+  //   console.log("SGT Concurrent Requests: ", result);
+  // });
+  // await testConcurrentRequests(LLAMA_NODES_URL).then((result) => {
+  //   console.log("Llama Nodes Concurrent Requests: ", result);
+  // });
+  // await testConcurrentRequests(TENDERLY_URL).then((result) => {
+  //   console.log("Tenderly Web3 Gateway Concurrent Requests: ", result);
+  // });
+  // await testConcurrentRequests(QUICKNODE_URL).then((result) => {
+  //   console.log("QuickNode Concurrent Requests: ", result);
+  // });
+})();
---
+++ rpc-profiler/secrets.js
@@ -0,0 +1,16 @@
+// Rename this to `secrets.js` and fill in the values
+
+const ALCHEMY_KEY = ""; // Add your Alchemy API key here
+const INFURA_KEY = "2b150eabf65140efb3d5508a888ee93e"; // Add your Infura API key here
+const CHAINSTACK_ENDPOINT = ""; // Add your Chainstack endpoint here
+const LLAMA_NODES_KEY = ""; // Add your Llama Nodes API key here
+const TENDERLY_KEY = ""; // Add your Tenderly Web3 Gateway Access key here
+const QUICKNODE_ENDPOINT = ""; // Add you QuickNode Endpoint here
+const CUSTOM_ENDPT = "https://rpc.sharedtools.org/rpc";
+
+exports.ALCHEMY_KEY = ALCHEMY_KEY;
+exports.INFURA_KEY = INFURA_KEY;
+exports.CHAINSTACK_ENDPOINT = CUSTOM_ENDPT;
+exports.LLAMA_NODES_KEY = LLAMA_NODES_KEY;
+exports.TENDERLY_KEY = TENDERLY_KEY
+exports.QUICKNODE_ENDPOINT = QUICKNODE_ENDPOINT;
